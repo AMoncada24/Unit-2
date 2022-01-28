48031c311a4e8bdb45f5ce0ccad19b0505059712
Abel <amoncada24@students.bmchs.org>
2022-01-28 09:11:14 -0600

    finished

--- user_input_challenge.py
+++ user_input_challenge.py
@@ -11,3 +11,14 @@ many copies of the previous message. (Hint: order of operations exists in Python
 Print out that many copies of the previous message on separate lines. 
 (Hint: the string "\n" is the same as pressing the ENTER button)
 '''
+name = input("What is your name? ")
+number1 = int(input("Enter your age. "))
+val1 = int(input("How many times would you like the message to print? "))
+number2 = 100
+fix = -1
+result = number1-number2
+trueresult = result*fix
+year = 2022
+year100 = year+trueresult
+text = f'You, {name}, will turn 100 in {year100}. \n' * val1
+print(text)
