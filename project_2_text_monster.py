0f7a37d575a1abc0c0d8c4e1ac8f887af130ea30
Abel <amoncada24@students.bmchs.org>
2022-03-21 09:03:26 -0500

    worked

--- project_2_text_monster.py
+++ project_2_text_monster.py
@@ -10,32 +10,28 @@ year100 = year+trueresult
 text = f'You, {name}, will turn 100 in {year100}. \n' * val1
 print(text)
 
+floor_1 = ['nothing', 'nothing', 'stairs up']
+floor_2 = ['nothing', 'nothing', 'stairs up']
+floor_3 = ['prize', 'nothing', 'nothing']
 
+user_room = 0
+user_floor = floor_1
 
+if user_input == "down":
+current_room = user_floor[user_room]
+if current_room != "stairs down":
+print("Can't go downstairs; there are no stairs.")
 
+if dead = True:
+    print("You died.")
+else
+    print("Congratulations! You got the prize!")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+user_input = input("Would you like to play again? (y/n)")
+    if user_input == y :
+        dead = False
+    if user_input == n :
+        print("Goodbye!")
+            break
+    else
+        print("That is not a valid input.")
